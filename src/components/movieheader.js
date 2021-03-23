0f214296bba426e51a27998b49beae4869232c82
tramnewin <trmnguyen2608@gmail.com>
2021-03-23 12:57:24 -0600

    third commit

--- src/components/movieheader.js
+++ src/components/movieheader.js
@@ -4,7 +4,6 @@ import {LinkContainer} from 'react-router-bootstrap';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
 import {logoutUser} from "../actions/authActions";
-import NavbarHeader from "react-bootstrap/lib/NavbarHeader";
 
 class MovieHeader extends Component {
     logout() {
@@ -21,10 +20,10 @@ class MovieHeader extends Component {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav>
-                            <LinkContainer to="/movielist">
+                            <LinkContainer to="/movies">
                                 <Nav.Link disabled={!this.props.loggedIn}>Movie List</Nav.Link>
                             </LinkContainer>
-                            <LinkContainer to={'/movie/' + (this.props.selectedMovie ? this.props.selectedMovie._id : '')}>
+                            <LinkContainer to={'/movies/' + (this.props.selectedMovie ? this.props.selectedMovie._id : '')}>
                                 <Nav.Link disabled={!this.props.loggedIn}>Movie Detail</Nav.Link>
                             </LinkContainer>
                             <LinkContainer to="/signin">
