fa1c040c668530528a0d10655e311f8b0bc38ccd
tramnewin <trmnguyen2608@gmail.com>
2021-04-04 15:39:14 -0600

    fifth commit

--- src/App.js
+++ src/App.js
@@ -10,19 +10,19 @@ import store from './stores/store';
 
 function App() {
   return (
-    <div className="App">
-      <Provider store={store}>
-        <HashRouter>
-          <div>
-            <MovieHeader />
-            <Route exact path="/" render={()=><MovieList />}/>
-            <Route exact path="/movies" render={()=><MovieList />}/>
-            <Route exact path="/movies/:movieId" render={()=><Movie />}/>
-            <Route path="/signin" render={()=><Authentication />}/>
-          </div>
-        </HashRouter>
-      </Provider>
-    </div>
+      <div className="App">
+        <Provider store={store}>
+          <HashRouter>
+            <div>
+              <MovieHeader />
+              <Route exact path="/" render={()=><MovieList />}/>
+              <Route exact path="/movielist" render={()=><MovieList />}/>
+              <Route exact path="/movie/:movieId" render={()=><Movie />}/>
+              <Route path="/signin" render={()=><Authentication />}/>
+            </div>
+          </HashRouter>
+        </Provider>
+      </div>
   );
 }
 
